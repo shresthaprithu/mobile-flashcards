4d7dc8c50d965f4efb0defb07b489105fae2471a
Prithu Shrestha <shresthaprithu@gmail.com>
2019-12-02 22:50:14 +1100

    feat: add DeckList and Deck components with test data

---
+++ components/Deck.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Deck = () => {
+  return (
+      <View style={styles.deckContainer}>
+        <View>
+          <Text style={styles.deckText}>Deck 1</Text>
+        </View>
+        <View>
+          <Text style={styles.cardText}>3 cards</Text>
+        </View>
+      </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  deckContainer: {
+    alignItems: 'center',
+    flexBasis: 120
+  },
+  deckText: {
+    fontSize: 28
+  },
+  cardText: {
+    fontSize: 18,
+    color: 'gray'
+  }
+});
+
+export default Deck;
---
+++ components/DeckList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import Deck from './Deck';
+
+export class DeckList extends Component {
+  render() {
+    return (
+        <View>
+          <Deck />
+          <Deck />
+        </View>
+    );
+  }
+}
+
+export default DeckList;

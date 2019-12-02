64073ae2f102b0aeaf868a229f8a1c16d28dd616
Prithu Shrestha <shresthaprithu@gmail.com>
2019-12-02 22:35:54 +1100

    feat: added api methods and test UI for functionality validation

--- utils/api.js
+++ utils/api.js
@@ -3,12 +3,16 @@ import { decks } from './_DATA';
 
 const DECKS_STORAGE_KEY = 'MobileFlashcards:decks';
 
-export function getDecksOld() {
-  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(formatDeckResults);
+export function getData() {
+  return decks;
 }
 
 function formatDeckResults(results) {
   return results === null ? decks : JSON.parse(results);
+}
+
+export function getDecksOld() {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(formatDeckResults);
 }
 
 export async function getDecks() {
@@ -19,12 +23,59 @@ export async function getDecks() {
       AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(decks));
     }
     
-    return storeResults === null ? desks : JSON.parse(storeResults);
+    return storeResults === null ? decks : JSON.parse(storeResults);
   } catch (err) {
     console.log(err);
   }
 }
 
-export function getData() {
-  return decks;
+export async function getDeck(id) {
+  try {
+    const storeResults = await AsyncStorage.getItem(DECKS_STORAGE_KEY);
+    
+    return JSON.parse(storeResults)[id];
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function saveDeckTitle(title) {
+  try {
+    await AsyncStorage.mergeItem(
+        DECKS_STORAGE_KEY,
+        JSON.stringify({
+          [title]: {
+            title,
+            questions: []
+          }
+        })
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function addCardToDeck(title, card) {
+  try {
+    const deck = await getDeck(title);
+    
+    await AsyncStorage.mergeItem(
+        DECKS_STORAGE_KEY,
+        JSON.stringify({
+          [title]: {
+            questions: [...deck.questions].concat(card)
+          }
+        })
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function resetDecks() {
+  try {
+    await AsyncStorage.removeItem(DECKS_STORAGE_KEY);
+  } catch (err) {
+    console.log(err);
+  }
 }
